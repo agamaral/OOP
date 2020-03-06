d2cab83be401b3e20c6aeafe92cec771ee887f9d
Antônio Augusto Zamai Amaral <51514805+agamaral@users.noreply.github.com>
2020-03-06 14:40:53 -0300

    projeto agenda

---
+++ Agenda/pck/Contato.java
@@ -0,0 +1,17 @@
+package pck;
+
+public class Contato {
+    private String nome;
+    private String email;
+    private double numTelefone;
+
+    public Contato(String nome, String email, double numTelefone) {
+        this.nome = nome;
+        this.email = email;
+        this.numTelefone = numTelefone;
+    }
+
+    private String inserirContato() {
+        
+    }
+}
--- Agenda/pck/Main.java
+++ Agenda/pck/Main.java
@@ -1,5 +1,7 @@
+package pck;
+
 public class Main {
     public static void main(String args[]) {
-        
+        Contato contatos[] = new Contato();
     }
 }
