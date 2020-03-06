ff3038c9b85b97e80a1c00e3617f2955684a9c4b
Antônio Augusto Zamai Amaral <51514805+agamaral@users.noreply.github.com>
2020-03-06 14:26:52 -0300

    .

--- Lista_01/pckSeis/Main.java
+++ Lista_01/pckSeis/Main.java
@@ -1,7 +1,5 @@
-package pckSeis;
 import java.util.Scanner;
 
-public class Cruzadinha {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
@@ -21,5 +19,3 @@ public class Cruzadinha {
         }
         sc.close();
     }
-
-}
