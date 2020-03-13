ebb88fa323017d568f98984b46ee116d0c439a90
Antonio Amaral <am4r4u@gmail.com>
2020-03-13 15:42:50 -0300

    exer 2 da lista 5

--- Lista_05/ex2/Empresa.java
+++ Lista_05/ex2/Empresa.java
@@ -11,5 +11,28 @@ public class Empresa {
         this.CNPJ = CNPJ;
     }
 
-    
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCNPJ() {
+		return this.CNPJ;
+	}
+
+	public void setCNPJ(String CNPJ) {
+		this.CNPJ = CNPJ;
+	}
+
+	public void setDepartamentos(Departamento d) {
+		this.departamentos = departamentos;
+	}
+
+	public Departamento getDepartamentos() {
+		return this.departamentos;
+	}
+
 }
--- Lista_05/ex2/Funcionario.java
+++ Lista_05/ex2/Funcionario.java
@@ -5,5 +5,33 @@ public class Funcionario {
     private int salario;
     private String dataAdimissao;
 
-    
+    public Funcionario(String nome, int salario, String dataAdmissao) {
+        this.nome = nome;
+        this.salario = salario;
+        this.dataAdimissao = dataAdmissao;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getSalario() {
+        return this.salario;
+    }
+
+    public void setSalario(int salario) {
+        this.salario = salario;
+    }
+
+    public String getDataAdimissao() {
+        return this.dataAdimissao;
+    }
+
+    public void setDataAdimissao(String dataAdimissao) {
+        this.dataAdimissao = dataAdimissao;
+    }
 }
