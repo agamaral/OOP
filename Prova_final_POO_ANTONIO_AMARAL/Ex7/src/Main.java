2084ef8ee12505d34711ef5b0c87fd185741ae67
Antonio Amaral <am4r4u@outlook.com>
2021-03-17 09:07:14 -0300

    delete

--- Prova_final_POO_ANTONIO_AMARAL/Ex7/src/Main.java
+++
@@ -1,5 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Usina u1 = new Usina();
-    }
-}
--- Prova_final_POO_ANTONIO_AMARAL/Ex8/src/Commision.java
+++
@@ -1,6 +0,0 @@
-public interface Commision {
-    double COMMISSION_RATE = 0.10;
-    
-    public default void setVendas(double sales) {
-    }
-}
